fa12cf3e7e7bcb6ae7c00f926c45e57dad4c462f
jhonnyNavarro <jnnavarro@espe.edu.ec>
2022-08-13 00:22:00 -0500

    validaciones

--- 06-Code/Project V3.0/JavaScript/validationsGeneral.js
+++ 06-Code/Project V3.0/JavaScript/validationsGeneral.js
@@ -1,46 +1,61 @@
 
-function validateName()
-{
-    var expRegName= /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+function validateName() {
+    var expRegName = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
     var productName = document.getElementById("name");
-    if (!expRegName.exec(productName.value))
-    {
+    if (!expRegName.exec(productName.value)) {
         alert("Se acepta solo letras");
         return false;
 
-    }else 
-    {
+    } else {
         return true;
     }
 }
 
-function validateNumbers()
-{
-    var expRegNumbers= /^[1-9]+$/; 
+function validateNumbers() {
+    var expRegNumbers = /^[1-9]+$/;
     var numbers = document.getElementById("numbers");
-    if (!expRegNumbers.exec(numbers.value))
-    {
+    if (!expRegNumbers.exec(numbers.value)) {
         alert("Se acepta solo numeros");
         return false;
 
-    }else 
-    {
+    } else {
         return true;
     }
 }
 
-function validatePrices()
-{
-    var expRegPrices= /^-?[0-9][0-9,\.]+$/;
-    var prices= document.getElementById("prices");
-    if(!expRegPrices.exec(prices.value))
-    {
+function validatePrices() {
+    var expRegPrices = /^-?[0-9][0-9,\.]+$/;
+    var prices = document.getElementById("prices");
+    if (!expRegPrices.exec(prices.value)) {
         alert("Solo se acepta numeros");
         return false;
     }
-    else
-    {
+    else {
         return true;
     }
 
+}
+
+function validateid() {
+    var expRegid = /^[1-9]{10}$/;
+    var Personid = document.getElementById("id");
+    if (!expRegid.exec(Personid.value)) {
+        alert("Se acepta solo numeros mmvdf");
+        return false;
+
+    } else {
+        return true;
+    }
+}
+
+function validateNamePerson() {
+    var expRegNamePerson = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s][^[:space:]]+$/;
+    var PersonName = document.getElementById("nameR");
+    if (!expRegNamePerson.exec(PersonName.value)) {
+        alert("Se acepta solo letras");
+        return false;
+
+    } else {
+        return true;
+    }
 }
