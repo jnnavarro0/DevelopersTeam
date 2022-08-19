bc3578711f4c73518ff68a250ae4541dd44e2e68
cepogo <cepogo@espe.edu.ec>
2022-08-18 23:41:53 -0500

    ModificarJavaScript

--- 06-Code/Project V3.0/JavaScript/validationCreateAccount.js
+++ 06-Code/Project V3.0/JavaScript/validationCreateAccount.js
@@ -3,6 +3,7 @@ function validateid() {
     var Personid = document.getElementById("id");
     if (!expRegid.exec(Personid.value)) {
         alert("Formato incorrecto");
+        document.getElementById("id").value="";
         return false;
 
     } else {
@@ -15,6 +16,7 @@ function validateNamePerson() {
     var PersonName = document.getElementById("nameR");
     if (!expRegNamePerson.exec(PersonName.value)) {
         alert("Solo se aceptan letras");
+        document.getElementById("nameR").value="";
         return false;
 
     } else {
@@ -26,6 +28,7 @@ function validateDirection() {
     var PersonDirection = document.getElementById("address");
     if (!expRegDirection.exec(PersonDirection.value)) {
         alert("No se aceptan caracteres especiales");
+        document.getElementById("address").value="";
         return false;
 
     } else {
@@ -37,6 +40,7 @@ function validateNamePassword() {
     var PersonPassword = document.getElementById("password");
     if (!expRegPassword.exec(PersonPassword.value)) {
         alert("La contraseña debe de tener de 6 a 12 dígitos");
+        document.getElementById("password").value="";
         return false;
 
     } else {
